9050eaccef044f1ad249b48f6a34cc8f86c9e954
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-11 16:58:05 +0000

    created pages new and score - take attempt and check for game logic

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,53 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  ALPHABET = ('a'..'z').to_a
+
+  def generate_letters(letters_arr)
+    10.times do
+      letters_arr << ALPHABET[rand(ALPHABET.length)]
+    end
+
+    letters_arr
+  end
+
+  def valid_chars?(letters, attempt)
+    attempt = attempt.downcase.chars
+    attempt.all? { |char| attempt.count(char) <= letters.count(char)}
+  end
+
+  def valid_word(attempt)
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+    serialized_words = open(url).read
+    words = JSON.parse(serialized_words)
+
+    words['found']
+  end
+
+  def define_result(attempt, letters)
+    if !@found
+      @result = "Sorry, #{attempt} is not an English word."
+    elsif !@valid
+      @result = "Sorry, but #{attempt} can't be built out of #{letters.split(' ').join(', ')}"
+    else
+      @result = "Congratulations! #{attempt} is a valid English word!"
+    end
+
+    @result
+  end
+
+  def new
+    @letters = []
+    @letters = generate_letters(@letters)
+  end
+
+  def score
+    @attempt = params[:word]
+    @grid = params[:letters]
+    @found = valid_word(@attempt)
+    @valid = valid_chars?(@grid, @attempt)
+
+    @result = define_result(@attempt, @grid)
+  end
+end
